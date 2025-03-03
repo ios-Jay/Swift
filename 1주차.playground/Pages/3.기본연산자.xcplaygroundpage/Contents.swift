d9d4ae29ecdf66e3f33e668df96e8523d22b6b81
ios-Jay <ios.developer.jay@gmail.com>
2025-03-03 23:05:33 +0900

    1주차

---
+++ 1주차.playground/Pages/3.기본연산자.xcplaygroundpage/Contents.swift
@@ -0,0 +1,557 @@
+import UIKit
+
+/*:
+ ![0](0.jpeg)
+ */
+
+/*: # 기본연산자(Basic Operators)
+ ---
+ */
+
+/*: ## 1) 연산자 기초
+ - 연산자 기초: 연산자란? 연산자의 종류
+ ![1](1.jpeg)
+ ---
+ */
+
+/**=======================================================================
+ 
+ 연산자(operator)
+ 
+ - 값을 검사, 바꾸거나, 또는 조합하기 위해 사용하는 특수한 ‘기호(symbol)’나 ‘구절(phrase)’임
+ 
+ [피연산자의 갯수에 따른 연산자의 구분]
+ 
+ - 1.단항연산자: 단일 대상의 바로 앞 또는 바로 뒤에 위치하는 연산자
+ 
+     (예시) -a ; +b ; !a ; a... 등이 존재
+ 
+ - 2.이항연산자: 두개의 대상의 가운데 위치하여 검사 및 계산. (대부분 이항연산자 형태로 사용)
+ 
+     (예시) a + b ; a - b ; a >= b
+ 
+ - 3.삼항연산자: 세개의 대상을 조합하여 특수한 계산을 함. (단 한가지의 삼항연산자가 존재)
+ 
+     (예시) a ? b : c
+ 
+==========================================================================**/
+
+
+/*: ## 2) 기본연산자
+ - 할당 연산자(Assignment Operator)
+ ![2](2.jpeg)
+ ---
+ */
+// 할당연산자: 오른쪽의 값을 왼쪽에 대입
+
+// (왼쪽의 값을 오른쪽의 값으로 초기화 하거나 업데이트 함)
+
+var num1 = 100
+
+var num2 = 1000
+
+num1 = num2
+
+
+/*:
+ ---
+ - 산술 연산자(Arithmetic Operator): 사칙 연산 등 기본적으로 스위프트에 내장되어 있는 기능
+ ![3](3.jpeg)
+---
+ */
+/**====================================
+ 
+ [산술 (이항) 연산자]
+ 
+ - + : 더하기 연산자      (단항 사용도 가능: +a)
+ 
+ - - : 빼기 연산자       (단항 사용도 가능: -a)
+ 
+ - * : 곱하기 연산자(별표)
+ 
+ - / : 나누기 연산자(슬래시)
+ 
+ - % : 모듈로(나머지) 연산자 ⭐️
+ 
+=======================================**/
+
+var a = 456
+
+var b = 123
+
+
+
+a + b
+
+a - b
+
+a * b
+
+// ================================
+
+b / a   // 몫 구하기(정수끼리)
+    
+b % a   // 나머지 구하기(정수끼리)
+
+
+
+
+//:> 모듈로(%)연산자:  비전공자에게 익숙하지 않지만, 정말 많이 사용하는 개념 ⭐️
+
+
+/*:---
+ - 모듈로(%)의 사용 (🔸 Int 일때만 사용) ➞ 주로 사용하는 패턴의 예시
+ ![4](4.jpeg)
+ ---
+ */
+var yourWatchingTime: Int = 130
+
+var hour = yourWatchingTime / 60
+
+var minute = yourWatchingTime % 60
+
+print("당신은 \(yourWatchingTime)분. 즉, 총 \(hour) 시간 \(minute) 분을 시청 하셨습니다.")
+
+for i in 1...100 {
+    
+    if i % 7 != 0 {
+        
+        continue
+    }
+    
+    print("7의 배수: \(i)")
+    
+}
+
+
+enum RpsGame: Int {
+    
+    case rock
+    
+    case paper
+    
+    case scissors
+}
+
+let number = Int.random(in: 1...100) % 3
+
+var myRpsGame: RpsGame = RpsGame(rawValue: number)!
+
+
+var num = 100
+
+var array = ["1", "2", "3"]
+
+array[0]
+
+array[num % 3]
+
+/*: ---
+ - 나누기 사용은 항상 주의
+ ---
+ */
+
+//:> 프로그래밍에서 나누기 사용은 항상 주의
+a = 4
+
+b = 5
+
+var newResult: Double
+
+// newResult = a / b
+
+newResult = Double(a / b)
+
+newResult = Double(a) / Double(b)
+
+
+/*:---
+ * 복합 할당 연산자(Compound Assignment Operators)
+ ![6](6.jpeg)
+ ---
+ */
+var value = 0
+
+
+// value = value + 10
+
+value += 10
+
+
+// value = value - 5
+
+value -= 5
+
+
+// value = value * 2
+
+value *= 2
+
+
+// value = value / 2
+
+value /= 2
+
+
+// value = value % 2
+
+value %= 2
+
+/*:---
+ * 비교연산자(Comparison Operators): 결과적으로 문장의 평가값은 참/거짓으로 도출
+ ![7](7.jpeg)
+ ---
+ */
+
+a = 456
+
+b = 123
+
+// 같다(Equal to operator)
+
+a == b
+
+// 같지 않다(Not equal to operator)
+
+a != b
+
+// 크다(Greater than operator)
+
+a > b
+
+// 크거나 같다(Greater than or equal to operator)
+
+a >= b
+
+// 작다(Less than operator)
+
+a < b
+
+// 작거나 같다(Less than or equal to operator)
+
+a <= b
+
+
+
+
+var c = (b == 123)
+
+
+
+// if문 하고 결합되어서 주로 사용
+
+
+
+/*:---
+ * 논리 연산자(Logical Operators)
+ ![8](8.jpeg)
+ ---
+ */
+// ⭐️ Logical NOT Operator
+
+!true
+
+!false
+
+
+// Logical AND Operator(앤드, 그리고)
+
+true && true
+
+true && false
+
+false && true
+
+false && false
+
+
+// Logical OR Operator(또는)
+
+true || true
+
+true || false
+
+false || true
+
+false || false
+
+
+
+
+// 아이디도 6자리 이상이고, 비밀번호도 6자리 이상이다.
+
+
+// 하다보면 자연스럽게 이해하게 됨 ===> 스트레스 받지 마세요.
+
+
+
+// ! 상당히 많이 사용함 (앱을 만들때 직접 많이 써보면서 익숙해 질 것임)
+
+c = !true
+
+
+//: ---
+
+
+
+/*: ## 3) 접근연산자
+ - 접근연산자 - (.)점
+ ![10](10.jpeg)
+ ---
+ */
+// 접근 연산자라고 불리는, .(점)은 어떤 의미일까?
+
+Int.random(in: 1...3)
+
+var number1 = Int.random(in: 1...5)
+
+var name = "스티브"
+
+print(name.count)
+
+/*:
+ ![11](11.jpeg)
+ */
+
+
+
+
+/*:
+ ![12](12.jpeg)
+ */
+
+//: ---
+
+/*: ## 4) 삼항 연산자(Ternary Conditional Operator)
+ - 기본 형태 / if문보다 조금은 한정적인 형태로 사용 가능
+ ![13](13.jpeg)
+ ---
+ */
+// if / else문
+
+
+var score = 70
+
+
+if score >= 70 {
+    
+    print("Pass")
+    
+} else {
+    
+    print("Fail")
+}
+
+// 3항 연산자 (위의 if/else문과 완벽하게 동일)
+
+score >= 70 ? print("Pass") : print("Fail")
+
+// 한줄인 경우
+
+// 주로 값을 대입하는 경우 (주로 값에서 2가지에서 한가지를 선택하는 경우)
+
+/*:
+ ![14](14.jpeg)
+ */
+
+//:> 조건에 따라 선택이 두가지인 경우 -> 삼항연산자를 떠올리자
+
+/*:
+ ![15](15.jpeg)
+ */
+
+
+let profileName: String = "차비시몬스"
+
+
+profileName.count >= 5 ? print("1") : print("2")
+
+
+
+
+/*: ## 5) 범위 연산자(Range Operators)
+ - 범위연산자: 스위프트에서 숫자의 범위를 간편하게 표시할때 사용
+ ![16](16.jpeg)
+ ---
+ */
+/**===================================================
+ 
+- 아래의 숫자와 위의 숫자까지의 범위를 포함
+
+- 자체가 특별한 타입으로 정의되어있고, 의미하긴 하지만
+ 
+- 일반적으로 위와 같이 타입을 정의해서 사용하는 경우는 드물고,
+ 
+- 1) 반복문(for문)    2) 배열    3) switch문
+ 
+- 에서만 주로 활용하므로 위의 경우에 활용 방식을 이해하는 것이 더 중요
+
+<주의점>
+ 
+- 내림차순 형식으로 표기 불가능  12...0
+ 
+- 실수형식의 범위 표기도 가능하지만 사용하는 경우는 드뭄
+ 
+=====================================================**/
+
+
+let numbers = 1...10
+
+
+/*:
+ ---
+ - 1) Closed Range Operator(닫힌 범위연산자), One-Sided Ranges의 표기
+ ![17](17.jpeg)
+ ---
+ */
+let range = 1 ... 10     // ClosedRange<Int>
+
+let range1 = 1...      // PartialRangeFrom<Int>
+
+let range2 =  ...10    // PartialRangeThrough<Int>
+
+
+// One-Sided 표기시에는 숫자와 범위연산자를 붙여서 써야함
+
+/*:
+ ---
+ - 2) Half-Open Range Operator(반 개방 범위연산자), One-Sided Ranges의 표기
+ ---
+ */
+let rangeH = 1 ..< 10     // Range<Int>
+
+let rangeH1 =  ..<10    // PartialRangeUpTo<Int>
+
+
+// One-Sided 표기시에는 숫자와 범위연산자를 붙여서 써야함
+
+
+
+/*: ## 6) 범위 연산자의 활용
+ - 1) for문과 함께 사용
+ ---
+ */
+for index in 1...5 {
+    
+    print("\(index) * 5: (index * 5)")
+    
+}
+
+let namesArray = ["라민 야말", "다니 올모", "차비 시몬스", "가비"]
+
+
+let playersCount = namesArray.count
+
+
+
+for i in 0..<playersCount {
+    
+    print("Plyaer \(i + 1) is called \(namesArray[i])")
+    
+}
+
+/*:---
+ - 2) 배열의 서브스크립트 문법과 함께 사용
+ ---
+ */
+
+
+namesArray[...2]
+
+namesArray[..<1]
+
+
+/*:---
+ - 3) 스위치문의 케이스에서 사용
+ ---
+ */
+
+var myage: Int = Int.random(in: 10...49)
+
+
+switch myage {
+case 10...19:
+    print("10대입니다.")
+case 20...29:
+    print("20대입니다.")
+case 30...39:
+    print("30대입니다.")
+case 40...49:
+    print("40대입니다.")
+default:
+    print("그 이외의 경우는 존재하지 않습니다.")
+}
+
+
+var point = (1,3)
+
+
+switch point {
+case (0,0):
+    print("0,0은 원점 위에 있다.")
+case (-2...2, -2...2):
+    print("\(point.0), \(point.1)은 원점의 주위에 있다.")
+default:
+    print("점은 (\(point.0), \(point.1))에 위치한다.")
+}
+
+
+/*: ## 7) 패턴매칭 연산자(~=)
+ - 패턴매칭 연산자: 숫자가 범위내에 있는지 확인하는 연산자
+ ![18](18.jpeg)
+ ---
+ */
+
+let myRange = 1...100
+
+myRange ~= 30
+
+// 결과 값은 참과 거짓
+
+
+/*:---
+ - 10 <= n <= 100 이런 문장은 스위프트에서 성립하지 않는다.
+ ---
+ */
+var n = 20
+
+
+if n >= 10 && n <= 100 {
+    
+    print("10이상, 100이하입니다.")
+    
+}
+
+
+if 10...100 ~= n {
+    
+    print("10이상, 100이하입니다.")
+    
+}
+
+var yourAge = 33
+
+if 20...29 ~= yourAge {
+    print("20대 입니다.")
+} else if 30...39 ~= yourAge {
+    print("30대 입니다.")
+}
+
+
+// 스위치문은 내부적으로 패턴매칭 연산자로 구현이 되어있음
+
+switch yourAge {
+    
+case 20...29:
+    
+    print("20대 입니다.")
+    
+case 30...39:
+    
+    print("30대 입니다.")
+    
+default :
+    
+    break
+}
+
---
+++ 1주차.playground/Pages/4.조건문.xcplaygroundpage/Contents.swift
@@ -0,0 +1,564 @@
+import UIKit
+
+/*:
+ ![0](0.jpeg)
+ */
+
+//: # 프로그래밍의 기본원리와 조건문
+
+//: ---
+/*:
+ ![1](1.jpeg)
+ ![2](2.jpeg)
+ */
+
+//: ---
+
+/*: ## 1) 조건문(if문)
+ - 참(true) 또는 거짓(false)의 특정 조건에 따라 특정 코드만 실행하게 할 수 있는 문장
+ ![3](3.jpeg)
+ ---
+ */
+var number = 10
+
+
+if number < 10 {
+    
+    print("10보다 작다.")
+}
+
+/*:
+ ![4](4.jpeg)
+ */
+
+
+//:> "조건"의 자리에는 결과가 참 또는 거짓이 나와야 함
+
+/*:
+ ![5](5.jpeg)
+ */
+
+// 조건문(if - else)
+
+
+number = 20
+
+if number <= 10 {
+    
+    print("10보다 작거나 같다.")
+    
+} else { // else 문에는 조건이 오는 것이 아니다. else if와는 다름
+    
+    print("10보다 크다.")
+}
+
+
+// if - else if - else
+
+
+number = 30
+
+if number <= 10 {
+    print("10보다 작거나 같다.")
+} else if number <= 20 {
+    print("20보다 작거나 같다.")
+} else {
+    print("20보다 크고 30보다 작거나 같다.")
+}
+
+
+// if - else if
+
+
+number = 40
+
+if number <= 30 {
+    print("30보다 작거나 같다.")
+} else if number >= 30 {
+    print("30보다 크거나 같다.")
+}
+
+
+
+/*:---
+ - 1. if문에서 논리적인 구조 및 조건의 순서가 중요
+ ---
+ */
+// 논리적인 오류가 없도록 구성해야함 ==========
+
+var num: Int = 8
+
+
+if num >= 0 {
+    print("0 또는 양의 정수 입니다.")
+} else if num % 2 == 0 {
+    print("양의 짝수 입니다.")
+} else if num % 2 != 0 {
+    print("양의 홀수 입니다.")
+} else {
+    print("음의 정수입니다.")
+}
+
+// 올바르게 수정해 본다면? ================
+
+if num >= 0 {
+    
+    print("0 또는 양수입니다.")
+    
+    if num % 2 == 0 {
+        
+        print("짝수입니다.")
+        
+    } else if num % 2 == 1 {
+        
+        print("홀수입니다.")
+    }
+    
+} else {
+    
+    print("음수입니다.")
+}
+
+
+// 조건의 확인 순서를 잘 고려해야함 ==========
+
+
+var score = 100
+
+
+if score >= 70 {
+    
+    print("70점이상입니다.")
+    
+} else if score >= 80 {
+    
+    print("80점이상입니다.")
+    
+} else if score >= 90 {
+    
+    print("90점이상입니다.")
+    
+} else {
+    
+    print("70점미만입니다.")
+}
+
+
+
+// 올바르게 수정해 본다면 ================
+
+
+if score >= 90 {
+    
+    // 범위가 작은 조건이 먼저 와야함
+    
+    print("90점이상입니다.")
+    
+} else if score >= 80 {
+    
+    print("80점이상입니다.")
+    
+} else if score >= 70 {
+    
+    print("70점이상입니다.")
+    
+} else {
+    
+    print("70점미만입니다.")
+}
+
+
+/*:---
+ - 2) 조건을 &&(and), 또는 ||(or)로 연결하는 것도 가능
+ ---
+ */
+var email = "a@gmail.com"
+
+var password = "1234"
+
+
+
+if email == "a@gmail.com" && password == "1234" {
+    
+    print("메인페이지로 이동")
+}
+
+
+
+if email != "a@gmail.com" || password != "1234" {
+    
+    print("경고메세지 띄우기")
+    
+    print("이메일주소 또는 패스워드가 올바르지 않습니다.")
+}
+
+
+
+/*:---
+ - 3) 중첩 사용 가능
+ ---
+ */
+var score1 = 81;
+
+if score1 >= 70 {
+    
+    if score1 == 100 {
+        
+        print("만점")
+        
+    } else {
+        
+        print("70점이상")
+    }
+    
+}
+
+
+//:> 프로그래밍에서는 타입만 일치하면 언제든지 중첩이 가능
+
+
+/*:---
+ - 참고 - 접근 연산자 (.)
+ ---
+ */
+
+// 한식.볶음밥.당근
+
+// 중식.짜장면.춘장
+
+var id = "abc"
+
+id.count
+
+"abcdef".count
+
+
+id.isEmpty
+
+id.dropFirst()
+
+//: ---
+
+/*: ## 2) 조건문(if문)의 활용
+ - 언제 사용할까? 활용예시
+ ![6](6.jpeg)
+ ---
+ */
+// 사용자의 아이디가 일치하고, 비밀번호도 일치 한다면
+
+// 입력된 아이디의 글자수가 6자리 이상이라면
+
+
+
+
+
+if id.count >= 6 {          //  .count는 내장된 기능 (지금은 몰라도 됨)
+    
+    print("아이디가 6글자 이상이네요. 다음 화면으로 넘어가겠습니다.")
+    
+}
+
+
+
+var myRpsChoice: Int = Int.random(in: 0...2)
+
+// 0:가위, 1:바위. 2:보
+
+if myRpsChoice == 0 {
+    print("가위입니다.")
+} else if myRpsChoice == 1 {
+    print("바위입니다.")
+} else if myRpsChoice == 2{
+    print("보입니다.")
+}
+
+//: ---
+
+/*: ## 3) Switch문
+ - 표현식/변수를 (매칭시켜) 분기처리할때 사용하는 조건문
+ ![7](7.jpeg)
+ ---
+ 
+ */
+
+
+
+//:> if문보다 한정적인 상황에서 사용
+/*:
+ ![8](8.jpeg)
+ */
+// 조건을 부등식이 아닌 "=="와만 비교
+
+// 변수가 어떤값을 가지냐에 따라 실행문을 선택하고 진행
+
+/*:
+ ![9](9.jpeg)
+ */
+
+
+var choice: String = "바위"
+
+
+switch choice { // 변수(표현식)
+case "가위":
+    print("가위입니다.") // "가위" == "가위"
+case "바위":
+    print("바위입니다.")
+case "보자기":
+    print("보자기입니다.")
+default:
+    break
+}
+
+/**=======================================================
+ 
+ [스위치문의 특징]
+ 
+ - 1)  스위치문에서 케이스의 ,(콤마)는 '또는'의 의미로 하나의 케이스에
+      여러 매칭값을 넣을 수 있음
+ 
+ - 2)  switch 문은 (비교하고 있는)값의 가능한 모든 경우의 수를 반드시
+      다루어야 함 (exhaustive: 하나도 빠뜨리는 것 없이 철저한)
+      모든 사례를 다루지 않았을 때에는 디폴트 케이스가 반드시 있어야 한다.
+ 
+ - 3)  각 케이스에는 문장이 최소 하나 이상 있어야 하며 만약 없다면 컴파일
+      에러 발생(의도하지 않은 실수를 방지 목적)
+ 
+ - 4)  실행하지 않으려면, break를 반드시 입력해야함 (if문에서는 실행문을
+      입력안해도 괜찮지만, 스위치문에서는 필요함)
+ 
+=========================================================**/
+
+
+switch choice {  // 문자열
+    
+case "가위":
+    
+    print("가위 입니다.")
+    
+case "바위", "보":
+    
+    print("바위 또는 보 입니다.")
+    
+default:
+    
+    break
+}
+
+
+
+var isTrue = true
+
+
+switch isTrue {
+    
+case true:
+    
+    print("true")
+    
+case false:
+    
+    print("false")
+    
+}
+
+
+
+/*:---
+ - fallthrough 키워드의 사용
+ ---
+ */
+
+var num1 = 10
+
+switch num1 {
+    
+case ..<10:
+    
+    print("1")
+    
+    // 매칭된 값에 대한 고려없이 무조건 다음블럭을 실행함
+    
+    fallthrough
+    
+case 10:
+    
+    print("2")
+    
+    fallthrough
+    
+default:
+    
+    print("3")
+    
+}
+
+//: ---
+
+/*: ## 4) Switch문의 활용
+ - 스위치문과 밸류바인딩(Value Binding)
+ ![11](11.jpeg)
+ ---
+ */
+// 바인딩의 일반적인 의미
+
+var n1 = 7
+
+
+let n2 = n1
+
+// 바인딩을 한다. (다른 변수 / 상수의 새로운 식별자로 할당한다.)
+
+
+//:> 바인딩: 다른 새로운 변수 / 상수 식별자로 할당
+
+var number1 = 6
+
+
+switch number1 {
+case let n:     // let n = number1
+    print("숫자: \(n)")
+default:
+    break
+}
+
+/*:---
+ - 스위치문과 where절(스위치문에서 조건을 확인하는 방식)
+ ---
+ */
+// 일단 다른 상수 값에 바인딩한(넣은)후, 조건절(참/거짓 문장)을 통해 다시 한번 더 조건 확인
+
+// (바인딩 된 상수는 케이스블럭 내부에서만 사용 가능하고, 상수 바인딩은 주로 where절하고 같이 사용됨)
+
+var number2 = 7
+
+switch number2 {
+case let n where n % 2 == 0:
+    print("짝수 숫자: \(n)")
+case let n where n % 2 == 1:
+    print("홀수 숫자: \(n)")
+default:
+    break
+}
+
+
+//:> where키워드는 조건을 확인하는 키워드
+
+/*:
+ ![12](12.jpeg)
+ */
+/*: ## 5) 연습문제 Ⅰ: 가위 바위 보 게임 만들기
+ 
+ - 가위 / 바위 / 보 어떻게 만들어야 할까?: 내가 선택한 것과 컴퓨터의 선택을 비교
+ ![13](13.jpeg)
+ ---
+ */
+
+
+/*:---
+ - if문으로만 만들어보기
+ ---
+ */
+
+var myChoice: Int = Int.random(in: 0...2)
+
+var comChoice: Int = Int.random(in: 0...2)
+
+if myChoice == 0 {
+    print("당신의 선택은 가위입니다.")
+} else if myChoice == 1 {
+    print("당신의 선택은 바위입니다.")
+} else if myChoice == 2 {
+    print("당신의 선택은 보입니다.")
+}
+
+if comChoice == 0 {
+    print("컴퓨터의 선택은 가위입니다.")
+} else if comChoice == 1 {
+    print("컴퓨터의 선택은 바위입니다.")
+} else if comChoice == 2 {
+    print("컴퓨터의 선택은 보입니다.")
+}
+
+
+if myChoice == comChoice {
+    print("비겼습니다.")
+} else if myChoice == 0 && comChoice == 2 {
+    print("당신이 이겼습니다.")
+} else if myChoice == 1 && comChoice == 0 {
+    print("당신이 이겼습니다.")
+} else if myChoice == 2 && comChoice == 1 {
+    print("당신이 이겼습니다.")
+} else {
+    print("당신은 졌습니다.")
+}
+
+
+
+
+
+/*:---
+ - Switch문과 함께 작성하기
+ ---
+ */
+
+var myChoice1: Int = Int.random(in: 0...2)
+
+var comChoice1: Int = Int.random(in: 0...2)
+
+
+switch myChoice1 {
+case 0:
+    print("당신의 선택은 가위입니다.")
+case 1:
+    print("당신의 선택은 바위입니다.")
+case 2:
+    print("당신의 선택은 보입니다.")
+default:
+    break
+}
+
+switch comChoice1 {
+case 0:
+    print("컴퓨터의 선택은 가위입니다.")
+case 1:
+    print("컴퓨터의 선택은 바위입니다.")
+case 2:
+    print("컴퓨터의 선택은 보입니다.")
+    
+default:
+    break
+}
+
+
+if myChoice == comChoice {
+    print("비겼습니다.")
+} else if myChoice == 0 && comChoice == 2 {
+    print("당신이 이겼습니다.")
+} else if myChoice == 1 && comChoice == 0 {
+    print("당신이 이겼습니다.")
+} else if myChoice == 2 && comChoice == 1 {
+    print("당신이 이겼습니다.")
+} else {
+    print("당신은 졌습니다.")
+}
+
+//: ---
+
+/*: ## 6) 연습문제 Ⅱ: 랜덤빙고 게임 만들기
+ - 내가 선택한 것과 컴퓨터가 랜덤으로 선택한 것을 비교하고 출력
+ ![14](14.jpeg)
+ ---
+ */
+var myChoice2: Int = 4
+
+var comChoice2: Int = Int.random(in: 1...10)
+
+if comChoice2 > myChoice2 {
+    print("Up")
+} else if comChoice2 < myChoice2 {
+    print("Down")
+} else {
+    print("Bingo")
+}
+
